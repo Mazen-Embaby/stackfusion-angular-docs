c5e74ec10480edab549b2eb0742234e844e0e299
Mazen Embaby <eng.mazen.embaby@gmail.com>
2025-08-20 23:34:12 +0400

    refactor: improve dropdown header

--- src/app/home/layout/header/header.component.ts
+++ src/app/home/layout/header/header.component.ts
@@ -1,21 +1,19 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavigationRoute } from './navigation.model';
 
 import { RouterLink } from '@angular/router';
 import { ToggleDarkModeComponent } from '../toggle-dark-mode/toggle-dark-mode.component';
-import { Dropdown } from 'flowbite';
 import { APP_INFO } from '../../../app-info';
+import { Dropdown, DropdownContent } from '@sf/dropdown';
+import { NgpMenuTrigger } from 'ng-primitives/menu';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, ToggleDarkModeComponent],
+  imports: [RouterLink, ToggleDarkModeComponent, Dropdown, DropdownContent, NgpMenuTrigger],
   styleUrls: ['./header.component.scss'],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent implements AfterViewInit {
-  @ViewChild('navBarBtn', { static: false }) navBarBtn!: ElementRef;
-  @ViewChild('navBarDropdown', { static: false }) navBarDropdown!: ElementRef;
-
+export class HeaderComponent {
   readonly info = APP_INFO;
   navBarDropdownFlow!: Dropdown;
 
@@ -24,11 +22,4 @@ export class HeaderComponent implements AfterViewInit {
     { name: 'Template', link: `${this.info.url}/template` },
     { name: 'Changelog', link: `${this.info.url}/changelog` },
   ];
-
-  ngAfterViewInit(): void {
-    this.navBarDropdownFlow = new Dropdown(this.navBarDropdown.nativeElement, this.navBarBtn.nativeElement);
-  }
-  hideMenu() {
-    this.navBarDropdownFlow.hide();
-  }
 }
